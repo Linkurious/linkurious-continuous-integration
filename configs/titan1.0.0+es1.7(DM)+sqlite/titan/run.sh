6b2c6701994227af520500372549a00791b7ee1d
Francesco Infante <fra.inf@icloud.com>
2016-10-11 11:36:49 +0200

    #4 Use dockerize in Titan

---
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bssh
+
+BIN=./bin
+
+#############################################################################
+# based on https://github.com/elubow/titan-gremlin/blob/master/run.sh       #
+# we use dockerize to wait on es and cassandra                              #
+#############################################################################
+
+# use cassandra backed db instead of berkeleyje
+sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
+sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
+sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChannelizer/channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer/g" conf/gremlin-server/gremlin-server.yaml
+
+# create the backing file
+echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
+storage.backend=cassandra
+storage.hostname=$CASSANDRA_PORT_9160_TCP_ADDR" > conf/gremlin-server/titan-cassandra-server.properties
+
+$BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
