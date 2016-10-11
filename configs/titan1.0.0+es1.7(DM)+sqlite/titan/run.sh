2ea678be0d34af0f2d6e0d04d6f26a1702a73938
Francesco Infante <fra.inf@icloud.com>
2016-10-11 11:44:09 +0200

    #4 Use ENTRYPOINT instead of CMD

--- configs/titan1.0.0+es1.7(DM)+sqlite/titan/run.sh
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run.sh
@@ -7,6 +7,8 @@ BIN=./bin
 # we use dockerize to wait on es and cassandra                              #
 #############################################################################
 
+dockerize -wait tcp://elasticsearch:9200 -wait tcp://cassandra:9160 -timeout 60s;
+
 # use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
 sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
