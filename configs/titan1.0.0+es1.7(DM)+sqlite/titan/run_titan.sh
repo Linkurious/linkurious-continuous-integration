9cba1f86f9392781e601b02e75892fda7459d869
Francesco Infante <fra.inf@icloud.com>
2016-10-12 15:21:54 +0200

    #4 Fix Titan, use sockets
    Look at
    https://github.com/davebshow/titan-gremlin/commit/7b7da36499ba4653ce0068
    c65f419d61faf8e6b8

--- configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
@@ -3,7 +3,7 @@
 BIN=./bin
 
 #############################################################################
-# based on https://github.com/elubow/titan-gremlin/blob/master/run.sh       #
+# based on https://github.com/davebshow/titan-gremlin/blob/master/run.sh    #
 # we use dockerize to wait on es and cassandra                              #
 #############################################################################
 
@@ -12,11 +12,10 @@ dockerize -wait tcp://elasticsearch:9200 -wait tcp://cassandra:9160 -timeout 60s
 # use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
 sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
-sed -i "s/channelizer: org.apache.tinkerpop.gremlin.server.channel.WebSocketChannelizer/channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer/g" conf/gremlin-server/gremlin-server.yaml
 
 # create the backing file
 echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
 storage.backend=cassandra
 storage.hostname=$CASSANDRA_PORT_9160_TCP_ADDR" > conf/gremlin-server/titan-cassandra-server.properties
 
-$BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
+$BIN/gremlin-server.sh conf/gremlin-server/gremlin-server-secure.yaml
