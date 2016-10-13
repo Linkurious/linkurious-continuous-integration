50799d47268bcd7dca1256abd6b38fa7a9cf3d17
Francesco Infante <fra.inf@icloud.com>
2016-10-13 16:25:40 +0200

    .

--- configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
-# sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
 
-./bin/titan.sh start; sleep 100000
+./bin/titan.sh start; sleep 1000
