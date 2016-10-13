e0c19c9a9e5f5ef837e53bb69d4c30be42a8228f
Francesco Infante <fra.inf@icloud.com>
2016-10-13 16:15:16 +0200

    test commit

--- configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
@@ -4,4 +4,4 @@
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
 # sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
 
-./bin/titan.sh start
+./bin/titan.sh start; sleep 100000
