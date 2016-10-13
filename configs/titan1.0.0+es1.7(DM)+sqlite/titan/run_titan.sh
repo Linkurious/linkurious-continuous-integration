6844595d4383000fe58f4846196f44360d7a9b52
Francesco Infante <francesco@linkurio.us>
2016-10-13 16:29:03 +0200

    Titan configuration (#11)

--- configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
+++ configs/titan1.0.0+es1.7(DM)+sqlite/titan/run_titan.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-BIN=./bin
-
-#############################################################################
-# based on https://github.com/davebshow/titan-gremlin/blob/master/run.sh    #
-# we use dockerize to wait on es and cassandra                              #
-#############################################################################
-
-dockerize -wait tcp://elasticsearch:9200 -wait tcp://cassandra:9160 -timeout 60s;
-
-# use cassandra backed db instead of berkeleyje
 sed -i "s/host: localhost/host: 0.0.0.0/g" conf/gremlin-server/gremlin-server.yaml
-sed -i "s/titan-berkeleyje-server.properties/titan-cassandra-server.properties/g" conf/gremlin-server/gremlin-server.yaml
 
-# create the backing file
-echo "gremlin.graph=com.thinkaurelius.titan.core.TitanFactory
-storage.backend=cassandra
-storage.hostname=cassandra" > conf/gremlin-server/titan-cassandra-server.properties
-
-$BIN/gremlin-server.sh conf/gremlin-server/gremlin-server.yaml
+./bin/titan.sh start; sleep 1000
