d05f310144dbf4baf8ea0c31cfe88094f7351720
Francesco Infante <fra.inf@icloud.com>
2016-10-14 10:42:33 +0200

    style

--- utils.js
+++ utils.js
@@ -15,7 +15,7 @@ const path = require('path');
  * @param {object} [options]
  */
 var exec = (cmd, options) => {
-  console.log('\x1b[36m', '> ' + cmd, '\x1b[0m');
+  console.log('\x1b[32m$ \x1b[0m' + cmd);
   require('child_process').execSync(cmd,
     _.defaults(options, {stdio: [0, 1, 2], shell: '/bin/bash'}));
 };
@@ -28,7 +28,7 @@ var exec = (cmd, options) => {
  * @returns {ChildProcess}
  */
 var execAsync = (cmd, options) => {
-  console.log('\x1b[36m', '> ' + cmd, '\x1b[0m');
+  console.log('\x1b[32m$ \x1b[0m' + cmd);
   return require('child_process').spawn(cmd,
     _.defaults(options, {shell: '/bin/bash'}));
 };
