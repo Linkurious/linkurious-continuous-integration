e622ea9ebc8826c517e7b4ed44c8971803354bac
Francesco Infante <fra.inf@icloud.com>
2016-10-20 11:53:17 +0200

    Fix debug print

--- test_server.js
+++ test_server.js
@@ -39,8 +39,8 @@ const clientBranch = exec('git ls-remote' +
   ? serverBranch
   : 'develop';
 
-console.log('\x1b[32m',
-  `Linkurious Server: ${serverBranch} and Test Linkurious Client: ${clientBranch}`, '\x1b[0m');
+console.log('\x1b[32m Test Linkurious Server: ' + serverBranch +
+  ' and Test Linkurious Client: ' + clientBranch + '\x1b[0m');
 
 // we read the last commit message to decide if we have to build or not
 const commitMessage = exec('git log -1 --pretty=%B', {stdio: null}).toString('utf8');
