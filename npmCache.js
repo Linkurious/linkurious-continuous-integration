b1daac8a55b094611696646b9d7fdb44d21444b2
Francesco Infante <fra.inf@icloud.com>
2016-12-19 17:11:25 +0100

    #23 wip

--- npmCache.js
+++ npmCache.js
@@ -3,10 +3,15 @@
  */
 'use strict';
 
+const fs = require('fs');
+const crypto = require('crypto');
+
 // locals
 const utils = require('./utils');
 
 const ciDir = process.env['CI_DIRECTORY'];
+
+const BUCKETS_ROOT_DIR = ciDir + '/tmp/npm-cache';
 
 class npmCache {
   /**
@@ -15,6 +20,7 @@ class npmCache {
    * @param {string} nodeModulesDir  destination path of npm install
    */
   constructor(packageJsonFile, binDir, nodeModulesDir) {
+    this.packageJsonFile = packageJsonFile;
     this.packageJsonData = require(packageJsonFile);
     this.binDir = binDir;
     this.nodeModulesDir = nodeModulesDir;
@@ -47,6 +53,10 @@ class npmCache {
   setNodeVersion(nodeVersion) {
     nodeVersion = nodeVersion || this.nodeVersion;
 
+    if (!nodeVersion) {
+      return; // desired version is not specified, system version is ok
+    }
+
     // download node globally
     utils.exec(`n ${nodeVersion} -d`, true);
     const nodePath = utils.exec(`n bin ${nodeVersion}`, true).split('\n')[0];
@@ -62,73 +72,76 @@ class npmCache {
   setNpmVersion(npmVersion) {
     npmVersion = npmVersion || this.npmVersion;
 
-    utils.exec(`npm install npm@${npmVersion}`);
+    if (!npmVersion) {
+      return;
+    }
+
+    utils.exec(`npm install npm@${npmVersion}`, true);
     utils.exec(`ln -sf ./node_modules/.bin/npm ${this.binDir}`, true);
+  }
+
+  /**
+   * Implicitly set the desired node and npm version.
+   * Look in the global cache if the same `package.json` file produced a previous
+   * node_modules directory. If not, run npm install on this package.json.
+   *
+   * Create and populate `this.nodeModulesDir` with the result of npm install.
+   * @param {object} [options] options
+   * @param {boolean} [options.ignoreScripts=false] whether to call npm install with the flag --ignore-scripts
+   * @returns {undefined}
+   */
+  install(options) {
+    options = options || {};
+
+    // install desired npm version
+    this.setNodeVersion();
+    this.setNpmVersion();
+
+    const hashPackageJson = crypto.createHash('md5')
+      .update(JSON.stringify(this.packageJsonData)).digest('hex');
+
+    const bucketDir = BUCKETS_ROOT_DIR + '/' + hashPackageJson;
+
+    let packageJsonDir = this.packageJsonFile.substring(
+      0, this.packageJsonFile.lastIndexOf('/'));
+
+    if (packageJsonDir === '') { packageJsonDir = '.'; }
+
+    try {
+      // does this directory exist?
+      fs.lstatSync(bucketDir + '/node_modules');
+
+      console.log('using cache');
+      // copy from the bucket to the packageJsonDir
+      utils.exec(`cp -r ${bucketDir}/node_modules ${packageJsonDir}/node_modules`);
+    } catch(e) {
+      if (e.code !== 'ENOENT') {
+        throw e;
+      }
+      // it doesn't exist we have to run npm install for this package.json
+
+      utils.exec('mkdir -p ' + bucketDir);
+
+      utils.changeDir(packageJsonDir, () => {
+        let flags = '';
+
+        if (options.ignoreScripts) {
+          flags += ' --ignore-scripts';
+        }
+
+        // we run npm install (the right node version is in /usr/local/bin)
+        utils.execRetry('npm install' + flags, 5);
+
+        // we copy the node_modules directory in our bucket
+        utils.exec(`cp -r ${packageJsonDir}/node_modules ${bucketDir}/node_modules`);
+
+        console.log('installing');
+        // there is no need to copy from the bucket to the repository since node_modules
+        // was created there
+      });
+    }
+
   }
 }
 
 module.exports = npmCache;
-
-/**
- * First, switch to the right node and npm version.
- * Hash the package.json file and look up if its node_modules directory was already cached.
- * If not, run npm install on this package.json.
- *
- * Return the absolute path to the node_modules directory.
- *
- * @param {string} packageJsonFile  path to the package.json file
- * @param {string} [nodeVersion]    node version
- * @param {string} [npmVersion]     npm version
- * @param {boolean} [ignoreScripts] whether to call npm install with the flag --ignore-scripts
- * @returns {string} absolute       path to the node_modules directory
-
-module.exports = (packageJsonFile, nodeVersion, npmVersion, ignoreScripts) => {
-  // we install the desired node version
-  if (nodeVersion) {
-    exec('n ' + nodeVersion);
-  }
-
-  // we install the desired npm version
-  if (npmVersion) {
-    execRetry('export PATH=/usr/local/bin:${PATH}; npm install -g npm@' + npmVersion, 5);
-  }
-
-  // hash the package.json file
-  var data = fs.readFileSync(packageJsonFile, 'utf8');
-  var hashPackageJson = crypto.createHash('md5').update(data).digest('hex');
-
-  // bucket containing the node_modules directory for this package.json
-  var directory = ciDir + '/tmp/npm-cache/' + hashPackageJson;
-
-  try {
-    // does this directory exist?
-    fs.lstatSync(directory + '/node_modules');
-  } catch(e) {
-    if (e.code !== 'ENOENT') {
-      throw e;
-    }
-    // it doesn't exist so we have to run npm install for this package.json
-
-    exec('mkdir -p ' + directory);
-
-    var packageJsonFolder = packageJsonFile.substring(0, packageJsonFile.lastIndexOf('/'));
-    if (packageJsonFolder === '') { packageJsonFolder = '.'; }
-
-    changeDir(packageJsonFolder, () => {
-      var flags = '';
-
-      if (ignoreScripts) {
-        flags += ' --ignore-scripts';
-      }
-
-      // we run npm install (the right node version is in /usr/local/bin)
-      execRetry('export PATH=/usr/local/bin:${PATH}; npm install' + flags, 5);
-
-      // we copy the node_modules directory in our bucket
-      exec(`cp -r ${packageJsonFolder}/node_modules ${directory}/node_modules`);
-    });
-  }
-
-  return directory + '/node_modules';
-};
-*/
