9f1b81d4228d65bf2ce4176caf53e073840ad5a6
Francesco Infante <fra.inf@icloud.com>
2016-12-22 14:59:26 +0100

    .

--- echidna.js
+++ echidna.js
@@ -323,7 +323,7 @@ class Echidna {
       console.log('out_of_docker');
 
       utils.exec('docker run -v /var/run/docker.sock:/var/run/docker.sock' +
-        ` -v ${ciDir}:/ci echidna sh -c "env IN_DOCKER=1 CI_DIRECTORY=/ci ./echidna.js ${cla}"`);
+        ` -v ${ciDir}:/ci echidna sh -c "env IN_DOCKER=1 CI_DIRECTORY=/ci /ci/echidna.js ${cla}"`);
     }
   }
 }
