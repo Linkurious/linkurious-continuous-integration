5f637c15c407d900b15151b48847a5660b595833
Francesco Infante <fra.inf@icloud.com>
2016-12-22 15:37:35 +0100

    .

--- utils.js
+++ utils.js
@@ -139,6 +139,7 @@ const getCurrentBranch = () => {
  * @returns {string} name of the repository
  */
 const getRepositoryName = () => {
+  console.log(process.cwd);
   let remoteOriginUrl = exec('git config --get remote.origin.url', true).trim();
 
   if (remoteOriginUrl.indexOf('https://github.com/') === 0 &&
